fb5f60f3a4209e9ffa78e6e70ed16db42f20585f
Aayush Bhan <aayushbhan@gmail.com>
2021-04-23 17:36:30 +0530

    Project Setup

---
+++ src/main/java/com/Visualizer/Stockopedia/StockopediaApplication.java
@@ -0,0 +1,13 @@
+package com.Visualizer.Stockopedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockopediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockopediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Visualizer/Stockopedia/StockopediaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Visualizer.Stockopedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockopediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
