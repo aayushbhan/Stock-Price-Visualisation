4699b137f8bcc8032144fb173d1c3256b61c0383
Aayush Bhan <aayushbhan@gmail.com>
2021-06-02 22:19:12 +0530

    Creation of producer and consumer

---
+++ src/main/java/com/Visualizer/Stockopedia/Service/Kafka/kafkaProducerService.java
@@ -0,0 +1,5 @@
+package com.Visualizer.Stockopedia.Service.Kafka;
+
+public interface kafkaProducerService {
+    void send(String message);
+}
--- src/main/java/com/Visualizer/Stockopedia/config/kafkaConfig.java
+++ src/main/java/com/Visualizer/Stockopedia/config/kafkaConfig.java
@@ -1,8 +1,12 @@
-package com.Visualizer.Stockopedia.Service.Kafka;
+package com.Visualizer.Stockopedia.config;
 
 import org.apache.kafka.clients.admin.AdminClientConfig;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaAdmin;
 
 import java.util.HashMap;
@@ -10,6 +14,8 @@ import java.util.Map;
 
 @Configuration
 public class kafkaConfig {
+
+    private static final Logger logger = LoggerFactory.getLogger(kafkaConfig.class);
 
     private static final String servers = "localhost:9092";
 
@@ -20,5 +26,12 @@ public class kafkaConfig {
         kafkaConfigs.put(AdminClientConfig.BOOTSTRAP_SERVERS_CONFIG,servers);
 
         return new KafkaAdmin(kafkaConfigs);
+    }
+
+    @Bean
+    public NewTopic stock1(){
+        logger.info("Created topic stock1");
+        return TopicBuilder.name("stock1")
+                .build();
     }
 }
